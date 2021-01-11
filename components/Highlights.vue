1cfdebb4db294bb52362f706de35ace9269ae600
priyanka <priya.pinky3103@gmail.com>
2021-01-11 18:44:50 -0400

    Added small components/changes in Highlights

--- components/Highlights.vue
+++ components/Highlights.vue
@@ -1,98 +1,241 @@
 <template>
     <!-- This example requires Tailwind CSS v2.0+ -->
-<div class="py-12 bg-white">
-  <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
-    <div class="lg:text-center">
-      <h2 class="text-2xl font-semibold tracking-wide text-indigo-600 uppercase">Mastering Interviews</h2>
-      <p class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
-        A smarter way to clear JOB interviews..
-      </p>
-      <p class="max-w-2xl mt-4 text-xl text-gray-500 lg:mx-auto">
-        PragraPrep empannels top indutry leaders to give your personalized interview guidance and strategy to convert the 
-        interview into opporunity.
-      </p>
+    <div class="py-12 bg-white">
+        <div class="px-4 mx-auto max-w-7xl sm:px-6 lg:px-8">
+            <div class="lg:text-center">
+                <h2 class="text-2xl font-semibold tracking-wide text-indigo-600 uppercase">
+                    Mastering Interviews
+                </h2>
+                <p class="mt-2 text-3xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-4xl">
+                    A smarter way to clear JOB interviews..
+                </p>
+                <p class="max-w-2xl mt-4 text-xl text-gray-500 lg:mx-auto">
+                    PragraPrep empannels top indutry leaders to give your personalized interview guidance and strategy to convert the interview into opporunity.
+                </p>
+            </div>
+
+            <div class="mt-10">
+                <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
+                                <!-- Heroicon name: globe-alt -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <dt class="text-lg font-medium leading-6 text-gray-800">
+                                1:1 Guidance
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
+                                <!-- Heroicon name: scale -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <dt class="text-lg font-medium leading-6 text-gray-900">
+                                Record & Reuse
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
+                                <!-- Heroicon name: lightning-bolt -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <dt class="text-lg font-medium leading-6 text-gray-900">
+                                Access to Question Bank
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
+                                <!-- Heroicon name: annotation -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4">
+                            <dt class="text-lg font-medium leading-6 text-gray-900">
+                                Confidential
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+                </dl>
+            </div>
+
+            <div class="lg:text-center mt-10">
+                <p class="mt-2 text-xl font-extrabold leading-8 tracking-tight text-gray-900 sm:text-xl">
+                    Hundreds of Tech Leaders at simple prices, starting at just $199/month.
+                </p>
+                <p class="max-w-2xl mt-4 text-xl text-gray-500 lg:mx-auto">
+                    PragraPrep empannels top indutry leaders to give your personalized interview guidance and strategy to convert the interview into opporunity.
+                </p>
+
+                <div class="flex flex-wrap items-center justify-center justify-center mt-10">
+                    <div class="rounded bg-grey-light w-64 p-2">
+                        <div class="flex justify-between border-black">
+                            <h3 class="text-sm">Ayla S</h3>
+                            <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
+                                <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+                            </div>
+                        </div>
+                        <div class="text-sm mt-2">
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Do a mobile first layout
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the meta tags
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the responsive layout on all devices
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="rounded bg-grey-light w-64 p-2">
+                        <div class="flex justify-between border-black">
+                            <h3 class="text-sm">Ayla S</h3>
+                            <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
+                                <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+                            </div>
+                        </div>
+                        <div class="text-sm mt-2">
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Do a mobile first layout
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the meta tags
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the responsive layout on all devices
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="rounded bg-grey-light w-64 p-2">
+                        <div class="flex justify-between border-black">
+                            <h3 class="text-sm">Ayla S</h3>
+                            <div class="text-grey-darker mt-2 ml-2 flex justify-between items-start">
+                                <img src="https://i.imgur.com/OZaT7jl.png" class="rounded-full" />
+                            </div>
+                        </div>
+                        <div class="text-sm mt-2">
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Do a mobile first layout
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the meta tags
+                            </div>
+
+                            <div class="bg-white p-2 rounded mt-1 border-b border-grey cursor-pointer hover:bg-grey-lighter">
+                                Check the responsive layout on all devices
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="m-20">
+                <dl class="space-y-10 md:space-y-0 md:grid md:gap-x-8 md:gap-y-10">
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-24 h-24 text-white bg-blue-500 rounded-full mr-6">
+                                <!-- Heroicon name: globe-alt -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path
+                                        stroke-linecap="round"
+                                        stroke-linejoin="round"
+                                        stroke-width="2"
+                                        d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3"
+                                    />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4 mt-2">
+                            <dt class="text-lg font-medium leading-6 text-gray-800">
+                                Free Trial
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-24 h-24 text-white bg-yellow-500 rounded-full mr-6">
+                                <!-- Heroicon name: scale -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                                    <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4 mt-2">
+                            <dt class="text-lg font-medium leading-6 text-gray-900">
+                                No Strings
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+
+                    <div class="flex">
+                        <div class="flex-shrink-0">
+                            <div class="flex items-center justify-center w-24 h-24 text-white bg-purple-500 rounded-full mr-6">
+                                <!-- Heroicon name: lightning-bolt -->
+                                <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
+                                </svg>
+                            </div>
+                        </div>
+                        <div class="ml-4 mt-2">
+                            <dt class="text-lg font-medium leading-6 text-gray-900">
+                                Money Back
+                            </dt>
+                            <dd class="mt-2 text-base text-gray-500">
+                                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
+                            </dd>
+                        </div>
+                    </div>
+                </dl>
+            </div>
+        </div>
     </div>
-
-    <div class="mt-10">
-      <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
-        <div class="flex">
-          <div class="flex-shrink-0">
-            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
-              <!-- Heroicon name: globe-alt -->
-            <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 4.354a4 4 0 110 5.292M15 21H3v-1a6 6 0 0112 0v1zm0 0h6v-1a6 6 0 00-9-5.197M13 7a4 4 0 11-8 0 4 4 0 018 0z" />
-</svg>
-            </div>
-          </div>
-          <div class="ml-4">
-            <dt class="text-lg font-medium leading-6 text-gray-800">
-              1:1 Guidance
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-            </dd>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="flex-shrink-0">
-            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
-              <!-- Heroicon name: scale -->
-              <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
-  <path d="M2 6a2 2 0 012-2h6a2 2 0 012 2v8a2 2 0 01-2 2H4a2 2 0 01-2-2V6zM14.553 7.106A1 1 0 0014 8v4a1 1 0 00.553.894l2 1A1 1 0 0018 13V7a1 1 0 00-1.447-.894l-2 1z" />
-</svg>
-            </div>
-          </div>
-          <div class="ml-4">
-            <dt class="text-lg font-medium leading-6 text-gray-900">
-              Record & Reuse
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-            </dd>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="flex-shrink-0">
-            <div class="flex items-center justify-center w-12 h-12 text-white bg-gray-500 rounded-md">
-              <!-- Heroicon name: lightning-bolt -->
-              <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 14v3m4-3v3m4-3v3M3 21h18M3 10h18M3 7l9-4 9 4M4 10h16v11H4V10z" />
-</svg>
-            </div>
-          </div>
-          <div class="ml-4">
-            <dt class="text-lg font-medium leading-6 text-gray-900">
-              Access to Question Bank
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-            </dd>
-          </div>
-        </div>
-
-        <div class="flex">
-          <div class="flex-shrink-0">
-            <div class="flex items-center justify-center w-12 h-12 text-white bg-indigo-500 rounded-md">
-              <!-- Heroicon name: annotation -->
-              <svg class="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-              </svg>
-            </div>
-          </div>
-          <div class="ml-4">
-            <dt class="text-lg font-medium leading-6 text-gray-900">
-              Confidential
-            </dt>
-            <dd class="mt-2 text-base text-gray-500">
-              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.
-            </dd>
-          </div>
-        </div>
-      </dl>
-    </div>
-  </div>
-</div>
-
 </template>
